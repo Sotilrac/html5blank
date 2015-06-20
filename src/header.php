ee50b2641fca8e894078d6f01cdb3da6ee1304c5
Carlos <carlos.asmat@gmail.com>
2015-06-20 19:18:29 -0400

    adds font awesome to bower, updates css and scss

--- src/header.php
+++ src/header.php
@@ -31,15 +31,14 @@
 			<!-- header -->
 			<header class="header clear" role="banner">
 
-					<!-- logo -->
-					<div class="logo">
+					<!-- title -->
+					<div class="title">
 						<a href="<?php echo home_url(); ?>">
-							<!-- svg logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script -->
-							<img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo" class="logo-img">
+							<h1><?php bloginfo('name'); ?></h1>
+						<p><?php bloginfo('description'); ?></p>
 						</a>
 					</div>
-					<!-- /logo -->
-
+					<!-- /title -->
 					<!-- nav -->
 					<nav class="nav" role="navigation">
 						<?php html5blank_nav(); ?>
